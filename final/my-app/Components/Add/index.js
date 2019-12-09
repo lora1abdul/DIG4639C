5c4d1a14c72cda7894de0ad9b4a577037acf5b5e
Lora Abdulhak <loraabdulhak@hotmail.com>
2019-12-08 21:03:43 -0500

    edit

--- final/my-app/Components/Add/index.js
+++ final/my-app/Components/Add/index.js
@@ -6,10 +6,6 @@ class Add extends React.Component {
 
   getValue = (event) => {
 
-    event.preventDefault();
-
-    const name = document.querySelector("#name").value;
-    const number = document.querySelector("#number").value;
 
     this.setState({"name": name, "number": number});
 
@@ -22,17 +18,12 @@ class Add extends React.Component {
       "headers": {
         "api": "abdulhak",
         },
-        
+
       "body": JSON.stringify({
         "name": this.state.added.name,
         "number": this.state.added.number
       })
-    })
-    .then(response => response.json() )
-    .then((data) => this.setState({added: data.added}) )
-    });
 
-  }
 
 
 export default Add;
